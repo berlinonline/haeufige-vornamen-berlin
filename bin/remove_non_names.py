7fba191c2ae5742f03daa597679a37e61c4fb1b4
Knud Möller <knud.moeller@berlinonline.de>
2023-09-07 10:17:11 +0200

    reimplement remove_non_names script in python, call from shell script

---
+++ bin/remove_non_names.py
@@ -0,0 +1,20 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Remove those lines from --source which start with any of the strings in --non_names.")
+parser.add_argument('--source',
+                    help="Path to the input CSV file")
+parser.add_argument('--non_names',
+                    help="Path to the list of non_names",
+                    default="")
+args = parser.parse_args()
+
+with open(args.non_names) as file:
+    non_names = [line.rstrip() for line in file]
+
+with open(args.source) as source_file:
+    for line in source_file:
+        line = line.rstrip()
+        matches = list(filter(line.startswith, non_names)) != []
+        if not matches:
+            print(line)
--- bin/remove_non_names.sh
+++ bin/remove_non_names.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# https://stackoverflow.com/a/17841619/9539770
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
 CSV_FOLDER=$1
-
-# strings that show up as names, but do not appear to be given names for various reasons
-non_names=("al" "bestimmt" "bin" "da" "de" "dela" "do" "el" "kein" "keinen" "la" "nicht" "noch" "noche" "oğlu" "Vorname" "Familienname" "\(Vorname[^;]*" "und" "ohne" "von" "van" "zur" "der" "del" "wegen" "Vornamen!" "(Eigenname)" "(Großvatersname)" "(Mittelname)" "(Vatersname)" "-" "'evič")
-# What about variants like gyzy, gizi, qizi, kyzy? What is that?
-
-
-non_name_list=$(join_by '|' "${non_names[@]}")
 for i in ${CSV_FOLDER}/*.csv;
     # filter out lines that start with one of the non_names
-    do egrep -v "^(${non_name_list})*," $i > $i.new ;
+    do python bin/remove_non_names.py --non_names conf/non_names.txt --source $i > $i.new ;
     mv $i.new $i
 done
